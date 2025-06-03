b0aa42ccefd84385d86356f3217197dfedb276ca
Hariom Singh <singh.hariom6199@gmail.com>
2025-06-03 22:44:10 +0530

    remove-element solved.

---
+++ array-string/remove-element.cpp
@@ -0,0 +1,46 @@
+// 🟠 Approach 1: 
+// Time Complexity: O(n)+O(n)
+// Space Complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0, n = nums.size();
+        for (int num : nums) {
+            if (num == val)
+                k++;
+        }
+
+        int i = 0, j = n - 1;
+        while (i < j) {
+            if (nums[j] == val) {
+                j--;
+            } else if (nums[i] == val && nums[j] != val) {
+                swap(nums[i++], nums[j--]);
+            } else {
+                i++;
+            }
+        }
+        return n - k;
+    }
+};
+
+// 🟢 Approach 2: Optimal In-Place 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+};
